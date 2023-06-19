1b74d29ea84595b66eedbf9cd5ee0b7fe27ae6c2
Mahdiware <mahdiware@gmail.com>
2023-06-19 16:21:37 +0300

    uncommented NT_Func

--- src/parse.h
+++ src/parse.h
@@ -8,22 +8,22 @@
 
 //if development is 1 then it is defined and displayed to the lexer and interpreter when running the code.
 #ifndef development
-	#define development 1
+	#define development 0
 #endif
 //capacity of variable & char length
 #ifndef VAR_CAPACITY
-	#define VAR_CAPACITY 128
+	#define VAR_CAPACITY 1024
 #endif
 
 enum {
     NT_Unit,   // Non-terminal symbol for the entire program or unit
     NT_Stmt,   // Non-terminal symbol for a statement
-    //NT_Iden, 	//
+    NT_Iden, 	//
     NT_Assn,   // Non-terminal symbol for an assignment statement
     NT_Prnt,   // Non-terminal symbol for a print statement
     NT_Inpt,   // Non-terminal symbol for an input statement
     NT_Ctrl,   // Non-terminal symbol for a control statement (e.g., if, while, do-while)
-    //NT_Func,	// Non-terminal symbol for an function statement
+    NT_Func,	// Non-terminal symbol for an function statement
     NT_Cond,   // Non-terminal symbol for a conditional expression (e.g., if condition)
     NT_Elif,   // Non-terminal symbol for an else-if clause
     NT_Else,   // Non-terminal symbol for an else clause
@@ -31,12 +31,12 @@ enum {
     NT_Whil,   // Non-terminal symbol for a while loop
     NT_Atom,   // Non-terminal symbol for an atomic expression
     NT_Expr,   // Non-terminal symbol for an expression
-    NT_Pexp,   // Non-terminal symbol for a primary expression
+    NT_Pexp,   // Non-terminal symbol for a Parenthesize expression
     NT_Bexp,   // Non-terminal symbol for a boolean expression
     NT_Uexp,   // Non-terminal symbol for a unary expression
     NT_Texp,   // Non-terminal symbol for a term expression
     NT_Aexp,   // Non-terminal symbol for an arithmetic expression
-    NT_COUNT   // Total count of non-terminal symbols
+    NT_COUNT,   // Total count of non-terminal symbols
 };
 
 struct token;
