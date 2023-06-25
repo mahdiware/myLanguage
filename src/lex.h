3ef4327adf3a81b63e32f378b80ea67d8ed6b1fd
Mahdiware <mahdiware@gmail.com>
2023-06-25 05:20:49 +0300

    src/lex.h: added enum TOKEN numbers

--- src/lex.h
+++ src/lex.h
@@ -16,51 +16,51 @@
 #define WHITE(s)          COLOURED(s, 1, 37)
 
 enum {
-    TK_NAME,   // Token for a name
-    TK_NMBR,   // Token for a number
-    TK_STRL,   // Token for a string literal
-    TK_WSPC,   // Token for whitespace
-    TK_LCOM,   // Token for a line comment
-    TK_BCOM,   // Token for a block comment
-    TK_TRUE,	
-    TK_FALSE,	
-    TK_NULL,	
-    TK_LPAR,   // Token for a left parenthesis '('
-    TK_RPAR,   // Token for a right parenthesis ')'
-    TK_LBRA,   // Token for a left bracket '['
-    TK_RBRA,   // Token for a right bracket ']'
-    TK_LBRC,   // Token for a left brace '{'
-    TK_RBRC,   // Token for a right brace '}'
-    TK_FUNC,	// Token for the 'function' keyword
-    TK_COND,   // Token for the 'if' keyword
-    TK_ELIF,   // Token for the 'elif' keyword
-    TK_ELSE,   // Token for the 'else' keyword
-    TK_DOWH,   // Token for the 'do-while' keyword
-    TK_WHIL,   // Token for the 'while' keyword
-    TK_ASSN,   // Token for the assignment operator '='
-    TK_EQUL,   // Token for the equality operator '=='
-    TK_NEQL,   // Token for the inequality operator '!='
-    TK_LTHN,   // Token for the less than operator '<'
-    TK_GTHN,   // Token for the greater than operator '>'
-    TK_LTEQ,   // Token for the less than or equal to operator '<='
-    TK_GTEQ,   // Token for the greater than or equal to operator '>='
-    TK_CONJ,   // Token for the logical conjunction operator '&&'
-    TK_DISJ,   // Token for the logical disjunction operator '||'
-    TK_PLUS,   // Token for the plus operator '+'
-    TK_MINS,   // Token for the minus operator '-'
-    TK_MULT,   // Token for the multiplication operator '*'
-    TK_DIVI,   // Token for the division operator '/'
-    TK_MODU,   // Token for the modulo operator '%'
-    TK_NEGA,   // Token for the logical negation operator '!'
-    TK_PRNT,   // Token for the 'print' keyword
-    TK_INPT,   // Token for the 'input' keyword
-    TK_SCOL,   // Token for the semicolon ';'
-    TK_QUES,   // Token for the question mark '?'
-    TK_COLN,   // Token for the colon ':'
-    TK_COMA,   // Token for the comma ','
-    TK_COUNT,  // Total number of tokens
-    TK_FBEG,   // Token for the beginning of a file
-    TK_FEND,   // Token for the end of a file
+           TK_NAME,   // Token for a name
+           TK_NMBR,   // Token for a number
+           TK_STRL,   // Token for a string literal
+           TK_WSPC,   // Token for whitespace
+           TK_LCOM,   // Token for a line comment
+  /*05*/  TK_BCOM,   // Token for a block comment
+           TK_TRUE,	  // Token for boolean true
+           TK_FALSE,	// Token for boolean false
+           TK_NULL,		// Token for string null
+           TK_LPAR,   // Token for a left parenthesis '('
+  /*10*/  TK_RPAR,   // Token for a right parenthesis ')'
+           TK_LBRA,   // Token for a left bracket '['
+           TK_RBRA,   // Token for a right bracket ']'
+           TK_LBRC,   // Token for a left brace '{'
+           TK_RBRC,   // Token for a right brace '}'
+  /*15*/  TK_FUNC,	// Token for the 'function' keyword
+           TK_COND,   // Token for the 'if' keyword
+           TK_ELIF,   // Token for the 'elif' keyword
+           TK_ELSE,   // Token for the 'else' keyword
+           TK_DOWH,   // Token for the 'do-while' keyword
+  /*20*/  TK_WHIL,   // Token for the 'while' keyword
+           TK_ASSN,   // Token for the assignment operator '='
+           TK_EQUL,   // Token for the equality operator '=='
+           TK_NEQL,   // Token for the inequality operator '!='
+           TK_LTHN,   // Token for the less than operator '<'
+  /*25*/  TK_GTHN,   // Token for the greater than operator '>'
+           TK_LTEQ,   // Token for the less than or equal to operator '<='
+           TK_GTEQ,   // Token for the greater than or equal to operator '>='
+           TK_CONJ,   // Token for the logical conjunction operator '&&'
+           TK_DISJ,   // Token for the logical disjunction operator '||'
+  /*30*/  TK_PLUS,   // Token for the plus operator '+'
+           TK_MINS,   // Token for the minus operator '-'
+           TK_MULT,   // Token for the multiplication operator '*'
+           TK_DIVI,   // Token for the division operator '/'
+           TK_MODU,   // Token for the modulo operator '%'
+  /*35*/  TK_NEGA,   // Token for the logical negation operator '!'
+           TK_PRNT,   // Token for the 'print' keyword
+           TK_INPT,   // Token for the 'input' keyword
+           TK_SCOL,   // Token for the semicolon ';'
+           TK_QUES,   // Token for the question mark '?'
+  /*40*/  TK_COLN,   // Token for the colon ':'
+           TK_COMA,   // Token for the comma ','
+           TK_COUNT,  // Total number of tokens
+           TK_FBEG,   // Token for the beginning of a file
+  /*44*/  TK_FEND,   // Token for the end of a file
 };
 
 struct token {
